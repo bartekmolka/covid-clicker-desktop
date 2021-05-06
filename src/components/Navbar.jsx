b83a52cb6720c1a7125493f28e6001e549ca37d3
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-05-06 13:29:54 +0200

    Motyw ciemny

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import NavLink from "./NavLink";
 import * as styles from "../styles/Navbar.module.css"
+import ThemeSwitch from "./ThemeSwitch";
 
 export default function Navbar()
 {
     return(
         <nav className={styles.main}>
             <div className={styles.burger}>
-                    <div classname={styles.line1}></div>
-                    <div classname={styles.line2}></div>
-                    <div classname={styles.line3}></div>
+                    <div className={styles.line1}></div>
+                    <div className={styles.line2}></div>
+                    <div className={styles.line3}></div>
             </div>  
             <ul className={styles.list}>
                 <NavLink name="Covid clicker" link="/"/>
                 <NavLink name="O nas" link="/"/>
                 <NavLink name="Kontakt" link="/"/>
+                <ThemeSwitch/>
             </ul>
             
             {/* Piotrek wstaw tu logo */}
