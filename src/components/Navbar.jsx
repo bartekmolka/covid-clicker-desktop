4fb6d696229f0b51a96b576754a62f71f012e605
n00sek <74304320+n00sek@users.noreply.github.com>
2021-05-07 10:39:14 +0200

    Dodano działanie "burgera" do nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import NavLink from "./NavLink";
 import * as styles from "../styles/Navbar.module.css"
 import ThemeSwitch from "./ThemeSwitch";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar()
 {
+    const [isRel, setIsRel] = useState(false);
+    
     return(
-        <nav className={styles.main}>
-            <div className={styles.burger}>
+        <nav className={isRel ? styles.main + " " + styles.mainon : styles.main}>
+            <div className={styles.burger} onClick={() => setIsRel(!isRel)}>
                     <div className={styles.line1}></div>
                     <div className={styles.line2}></div>
                     <div className={styles.line3}></div>
             </div>  
-            <ul className={styles.list}>
+            <ul className={isRel ? styles.mainon : styles.list}>
                 <NavLink name="Covid clicker" link="/"/>
                 <NavLink name="O nas" link="/"/>
                 <NavLink name="Kontakt" link="/Kontakt.html"/>
