0378286256738624feb4df354c51e7ccef3c0dd1
n00sek <74304320+n00sek@users.noreply.github.com>
2021-05-07 14:44:16 +0200

    Dodano stopkę

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -3,7 +3,7 @@ import * as styles from "../styles/Foot.module.css"
 export default function Foot()
 {
     return (
-        <div className="foot">
+        <div className={styles.foot}>
             {/* Stopkę robi mistrz od stopek - Paweł */}
         </div>
     )
