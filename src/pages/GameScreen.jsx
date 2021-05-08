ec1e7d81d1b875065f9e098e4aca1ea66d16e663
n00sek <74304320+n00sek@users.noreply.github.com>
2021-05-08 19:29:17 +0200

    Ustawiono cel jako dzienne, nowe przypadki zarażenia COVID19

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -3,20 +3,20 @@ import * as styles from "../styles/GameScreen.module.css"
 import ClickCounter from "../components/ClickCounter"
 import Virus from "../components/Virus"
 import SideBar from "../components/SideBar"
-var f
-var b
-var c 
+
+let newCases
 const date = new Date()
 
 
-var valu = "2021-"+date.getMonth()+"-"+date.getDate()
-fetch("https://api.covid19api.com/world/total")
+var valuT = "2021-"+(date.getMonth()+1)+"-"+(date.getDate()-1)
+var valuY = "2021-"+(date.getMonth()+1)+"-"+(date.getDate()-2)
+
+console.log(valuY, valuT)
+
+    fetch("https://api.covid19api.com/country/poland/status/confirmed?from="+valuY+"T00:00:00Z&to="+valuT+"T00:00:00Z")
         .then(res=>res.json())
-        .then(data=> b =  data["TotalConfirmed"]
-        )
-    fetch("https://api.covid19api.com/total/country/poland/status/confirmed?from="+valu+"T00:00:00Z&to="+valu+"T01:00:00Z")
-    .then(res=>res.json())
-.then(res=>c =res[0]["Cases"])
+        .then(res=> newCases =res[1]["Cases"] - res[0]["Cases"])
+    
 
 export default function GameScreen() {
     let [clicks, setClicks] = useState(0)
@@ -26,7 +26,7 @@ export default function GameScreen() {
         <div className="screen">
             <div className={styles.main}>
                 <Virus className={styles.virus} toggle={OnVirusClick}/>
-                <ClickCounter clickCount={clicks} clicksToWin={Math.round(c/1000)} time={`Czas: ${Math.floor(Time/1000)}s`}/>
+                <ClickCounter clickCount={clicks} clicksToWin={newCases} time={`Czas: ${Math.floor(Time/1000)}s`}/>
             </div>
             <SideBar/>
   
