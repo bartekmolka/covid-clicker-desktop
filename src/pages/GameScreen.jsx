69364dec6101e06653ec956f02e25a19ec188dbb
bart016 <bart.turczynski@gmail.com>
2021-05-08 13:34:14 +0200

    ustawiam cel
    cel kliknięć wynosi obecną liczbę potwierdzonych przypadków covid-19 w polsce

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -3,13 +3,21 @@ import * as styles from "../styles/GameScreen.module.css"
 import ClickCounter from "../components/ClickCounter"
 import Virus from "../components/Virus"
 import SideBar from "../components/SideBar"
-var b
 
+var b
+var c 
+const date = new Date()
+
+
+var valu = "2021-"+date.getMonth()+"-"+date.getDate()
 fetch("https://api.covid19api.com/world/total")
         .then(res=>res.json())
         .then(data=> b =  data["TotalConfirmed"]
         )
-        
+    fetch("https://api.covid19api.com/total/country/poland/status/confirmed?from="+valu+"T00:00:00Z&to="+valu+"T01:00:00Z")
+    .then(res=>res.json())
+.then(res=>c =res[0]["Cases"])
+
 export default function GameScreen() {
     let [clicks, setClicks] = useState(0)
     let [Time, setTime] = useState(0)
@@ -18,7 +26,7 @@ export default function GameScreen() {
         <div className="screen">
             <div className={styles.main}>
                 <Virus className={styles.virus} toggle={OnVirusClick}/>
-                <ClickCounter clickCount={clicks} clicksToWin={b} time={`Czas: ${Math.floor(Time/1000)}s`}/>
+                <ClickCounter clickCount={clicks} clicksToWin={c} time={`Czas: ${Math.floor(Time/1000)}s`}/>
             </div>
             <SideBar/>
   
