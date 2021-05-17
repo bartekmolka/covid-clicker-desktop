4caff58f131f40af9a632b274141afcf8cb60875
bart016 <bart.turczynski@gmail.com>
2021-05-17 19:22:47 +0200

    test
    st

--- src/components/PowerUpsTab.jsx
+++ src/components/PowerUpsTab.jsx
@@ -3,10 +3,10 @@ import data from "../PowerUps.json";
 import PowerUp from "./PowerUp";
 
 export default function PowerUpsTab({onUpdate, clicks})
-{
+{       let a=1;
     return(
         <div className={styles.tab}>
-            {data.map((e, i) => <PowerUp key={i} name={e.name} desc={e.desc} price={e.price} multi={e.multi} photo={e.photo} canBuy={e.price <= clicks} onBuy={() => onUpdate(e.price, e.multi)}/>)}
+            {data.map((e, i) => <PowerUp key={i} name={e.name} desc={e.desc} price={e.price} multi={e.multi} photo={e.photo} canBuy={e.price <= clicks} onBuy={() => onUpdate( e.price, e.multi, e.multi==0?(new Audio ("never_gonna.mp3")).play():"")}/>)}
         </div>
     )
 }
