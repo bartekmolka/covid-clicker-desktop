a353738f630bb3b5cccd53f0347382ff64b11dd0
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-05-21 21:34:30 +0200

    Dodano podstronę pobierania

---
+++ src/pages/Download.jsx
@@ -0,0 +1,10 @@
+import * as styles from "../styles/Download.module.css"
+
+export default function Download()
+{
+    return(
+        <div className="download">
+            {/* Miejsce dla Pawła */}
+        </div>
+    )
+}
